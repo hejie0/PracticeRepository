2deb374e1141007bdf3d53da05890b0d2ec78302
githubHejie <2251646333@qq.com>
2019-12-02 15:40:31 +0800

    删除base64多余代码

--- utils/src/main/java/encryption/jdk8/Base64Util.java
+++ utils/src/main/java/encryption/jdk8/Base64Util.java
@@ -1,8 +1,6 @@
 package encryption.jdk8;
 
-import java.text.SimpleDateFormat;
 import java.util.Base64;
-import java.util.Date;
 
 public class Base64Util {
 
@@ -36,17 +34,5 @@ public class Base64Util {
         System.out.println(encodedText);
         //解码
         System.out.println(new String(decode(encodedText), "UTF-8"));
-
-        String expiryStr = "2019-11-28 10:51:10";
-        String currentStr = "2019-11-28 10:52:10";
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date expiryDate = sdf.parse(expiryStr);
-        Date currentDate = new Date(); //sdf.parse(currentStr);
-        long time = System.currentTimeMillis() / 1000 + 2 * 24 * 60 * 60;
-
-        System.out.println("expiryDate: " + expiryDate.getTime());
-        System.out.println("currentDate: " + currentDate.getTime());
-        System.out.println("currentDate >= expiryDate: " + (currentDate.getTime() >= expiryDate.getTime()));
-        System.out.println();
     }
 }
