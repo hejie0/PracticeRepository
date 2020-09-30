1ae6be1b4a97f85779a952df86c6318937294291
githubHejie <2251646333@qq.com>
2020-09-30 14:33:29 +0800

    a

---
+++ srping-cloud-config-server/src/test/java/com/future/srpingcloudconfigserver/SrpingCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.future.srpingcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
