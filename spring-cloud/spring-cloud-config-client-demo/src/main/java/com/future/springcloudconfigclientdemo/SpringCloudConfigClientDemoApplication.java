e33eedfded8eb2534c9ee13238b49db56362666f
github <2251646333@qq.com>
2020-11-19 14:58:25 +0800

    删除spring-cloud-config-client和spring-cloud-config-server项目

--- spring-cloud/spring-cloud-config-client-demo/src/main/java/com/future/springcloudconfigclientdemo/SpringCloudConfigClientDemoApplication.java
+++ spring-cloud/spring-cloud-config-client-demo/src/main/java/com/future/springcloudconfigclientdemo/SpringCloudConfigClientDemoApplication.java
@@ -2,6 +2,16 @@ package com.future.springcloudconfigclientdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.PropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class SpringCloudConfigClientDemoApplication {
@@ -10,4 +20,16 @@ public class SpringCloudConfigClientDemoApplication {
 		SpringApplication.run(SpringCloudConfigClientDemoApplication.class, args);
 	}
 
+	@Configuration
+	@Order(Ordered.HIGHEST_PRECEDENCE)
+	public static class MyPropertySource implements PropertySourceLocator {
+
+		@Override
+		public PropertySource<?> locate(Environment environment) {
+			Map<String, Object> source = new HashMap<>();
+			source.put("server.port", 8080);
+			MapPropertySource propertySource = new MapPropertySource("my-property-source", source);
+			return propertySource;
+		}
+	}
 }
